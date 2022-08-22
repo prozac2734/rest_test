aa52be7516309564c9b07f2a4a2fa5f60a8f535b
prozac2734 <prozac2734@gmail.com>
2022-08-22 10:11:24 +0800

    patch 2

--- sw.js
+++ sw.js
@@ -4,3 +4,10 @@ self.addEventListener('install', (e) => {
     console.log('[Service Worker] Caching all: app shell and content');
   })());
 });
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
